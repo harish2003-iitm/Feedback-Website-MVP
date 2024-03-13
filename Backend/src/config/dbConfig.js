5005c3eae820254c753dc31e2c8209b80f401580
G.K.Harish Balaji <95614337+harish2003-iitm@users.noreply.github.com>
2024-03-13 19:26:36 +0530

    Update dbConfig.js

--- Backend/src/config/dbConfig.js
+++ Backend/src/config/dbConfig.js
@@ -3,9 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'sys', // Replace with your actual database name
-    password: '^N_v[ESt-n8', // Replace with your actual password for the root user
-    port: 3306,
+    database: 'sys', 
+    password: '^N_v[ESt-n8',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
